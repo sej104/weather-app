e2f0572a77b337d91972eaecbfd5d3add364d4c4
Sergio Joya <sejoya104@gmail.com>
2025-01-01 12:22:50 -0500

    Add function to fetch weather data

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-import '../styles.css'
+const fetchWeather = async (location) => {
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5ARRSV5UWBFTPL6GB73CPCH47`;
+    const response = await fetch(url, { mode: "cors" });
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const searchWeather = (e) => {
+  e.preventDefault();
+  const input = document.querySelector("input");
+  fetchWeather(input.value).then((data) => console.log(data));
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", searchWeather);
