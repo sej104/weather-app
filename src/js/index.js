f4e8e991a6ba1585b2a7c88de5c92e8fabe28a7e
Sergio Joya <sejoya104@gmail.com>
2025-01-02 09:33:56 -0500

    Add submit event listener

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,4 @@
-const fetchWeather = async (location) => {
-  try {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5ARRSV5UWBFTPL6GB73CPCH47`;
-    const response = await fetch(url, { mode: "cors" });
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const searchWeather = (e) => {
-  e.preventDefault();
-  const input = document.querySelector("input");
-  fetchWeather(input.value).then((data) => console.log(data));
-};
+import searchWeather from "./screenController.js";
 
 const form = document.querySelector("form");
 form.addEventListener("submit", searchWeather);
